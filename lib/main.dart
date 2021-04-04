b4a0f3f5eee520eeb2a9d2674ef906687ee687ad
Ahmed Hussein <ahmed0zakarea@gmail.com>
2021-04-04 20:04:31 +0300

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
-
+import 'package:ecommerce/Screens/splash_screen.dart';
 import 'package:flutter/material.dart';
-import './Screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,41 +12,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(fontFamily: 'Ubuntu'),
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    Timer(Duration(seconds: 3), () {
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
-        return HomeScreen();
-      }));
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Center(
-        child: Container(
-          height: size.height * 0.6,
-          width: size.height * 0.4,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('images/shopping.png'), fit: BoxFit.contain),
-          ),
-        ),
-      ),
     );
   }
 }
