4710cebd7a9e75d6e8b83fcecd5e53690b87f0c0
Ahmed Hussein <ahmed0zakarea@gmail.com>
2021-04-04 18:06:58 +0300

    Second Commit

--- lib/product.dart
+++ lib/product.dart
@@ -18,68 +18,71 @@ class Product extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.all(size.height * 0.0125),
-      height: size.height * 0.45,
-      width: size.height * 0.25,
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(size.height * 0.010)),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(size.height * 0.010),
-                child: Image.asset(
-                  image,
-                  fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: function,
+      child: Container(
+        margin: EdgeInsets.all(size.height * 0.0125),
+        height: size.height * 0.45,
+        width: size.height * 0.25,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(size.height * 0.010)),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(size.height * 0.010),
+                  child: Image.asset(
+                    image,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
+              flex: 3,
             ),
-            flex: 3,
-          ),
-          Expanded(
-              child: Container(
-            padding: EdgeInsets.symmetric(horizontal: size.height * 0.003),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Row(children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      name,
-                      style: TextStyle(
-                          fontSize: size.height * 0.020,
-                          fontWeight: FontWeight.bold),
-                      overflow: TextOverflow.ellipsis,
+            Expanded(
+                child: Container(
+              padding: EdgeInsets.symmetric(horizontal: size.height * 0.003),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Row(children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        name,
+                        style: TextStyle(
+                            fontSize: size.height * 0.020,
+                            fontWeight: FontWeight.bold),
+                        overflow: TextOverflow.ellipsis,
+                      ),
                     ),
-                  ),
-                ]),
-                Row(children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      title,
-                      style: TextStyle(color: Colors.grey.shade700),
-                      overflow: TextOverflow.ellipsis,
+                  ]),
+                  Row(children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        title,
+                        style: TextStyle(color: Colors.grey.shade700),
+                        overflow: TextOverflow.ellipsis,
+                      ),
                     ),
-                  ),
-                ]),
-                Row(children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      '\$' + '$price',
-                      style: TextStyle(
-                          fontSize: size.height * 0.020,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.green.shade500),
-                      overflow: TextOverflow.ellipsis,
+                  ]),
+                  Row(children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        '\$' + '$price',
+                        style: TextStyle(
+                            fontSize: size.height * 0.020,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green.shade500),
+                        overflow: TextOverflow.ellipsis,
+                      ),
                     ),
-                  ),
-                ]),
-              ],
-            ),
-          ))
-        ],
+                  ]),
+                ],
+              ),
+            ))
+          ],
+        ),
       ),
     );
   }
